f02f2e51ebe78a73412bee203aff0f27049cd361
elgazar04 <melgazar061@gmail.com>
2025-10-30 17:57:33 +0300

    Update homepage URL in package.json and set base path in vite.config.js for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Gaming-Freelance-Platform-UI1/', // 👈 ADD THIS LINE (very important)
   server: {
     port: 3000,
     open: true
   }
 })
-
