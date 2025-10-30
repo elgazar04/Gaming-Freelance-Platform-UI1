c690acfe76ea365d110f786769460ec085f43134
elgazar04 <melgazar061@gmail.com>
2025-10-30 19:18:07 +0300

    Fix base path in vite.config.js by adding a trailing slash for correct deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Gaming-Freelance-Platform-UI1', // 👈 ADD THIS LINE (very important)
+  base: '/Gaming-Freelance-Platform-UI1/', // 👈 ADD THIS LINE (very important)
   server: {
     port: 3000,
     open: true
